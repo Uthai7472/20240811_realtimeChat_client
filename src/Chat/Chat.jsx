0f98546d531adf6f4898a5825f9948c0e9294d88
Uthai7472 <141602580+Uthai7472@users.noreply.github.com>
2024-08-18 18:31:23 +0700

    Update Chat.jsx

--- src/Chat/Chat.jsx
+++ src/Chat/Chat.jsx
@@ -134,9 +134,9 @@ const Chat = ({ user }) => {
                     <div key={index}
                         className={`flex ${msg.sender_id === user.id ? 'items-end' : 'items-start'} flex-col`}
                     >
-                        <p className='font-bold'>{new Date(msg.created_at).toLocaleString()}</p>
                             {msg.sender_id === user.id ? (
-                                <div className='flex justify-end items-center w-[60%] rounded-[20%] border-[0.5px] border-black p-2'>
+                                <div className='flex flex-col justify-center items-end w-[60%] rounded-xl border-[0.5px] border-black p-2'>
+                                    <p className='font-bold'>{new Date(msg.created_at).toLocaleString()}</p>
                                     <p>
                                         {msg.message}
                                     </p>
@@ -148,7 +148,8 @@ const Chat = ({ user }) => {
                                     
                                 </div>
                             ) : (
-                                <div className='flex flex-col items-start w-[60%] rounded-[20%] border-[0.5px] border-black p-2'>
+                                <div className='flex flex-col items-start justify-center w-[60%] rounded-xl border-[0.5px] border-black p-2'>
+                                    <p className='font-bold'>{new Date(msg.created_at).toLocaleString()}</p>
                                     <p className='font-bold'>
                                             {msg.sender_id === user.id ? '' : `${friendUsername}`}
                                     </p>
